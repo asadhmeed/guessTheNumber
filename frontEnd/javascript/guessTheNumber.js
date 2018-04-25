b21c94ce783b6c52b60f4c9375985f219df56e24
asadhmeed <asadhmeed1@gimail.com>
2018-04-25 19:06:46 +0300

    frontend update

--- frontEnd/javascript/guessTheNumber.js
+++ frontEnd/javascript/guessTheNumber.js
@@ -12,12 +12,15 @@ const app = {
 // tasks
 // clear button
 // restart button
-// display the guesses in the table withe auto update and check the digets and display the corect digets and the wrong ones
+// display the guesses in the table with auto update and check the digits and display the corect digits and the wrong ones
 // display the high score table withe auto update
 
 
 function hideTheGameOnStart() {
-    setDisplayNone("game-grid")
+    setDisplayNone("admenPasswordId");
+    setDisplayNone("nameSubmet");
+    setDisplayNone("game-grid");
+
 }
 
 
@@ -28,90 +31,124 @@ function startTheGame() {
         setDisplayNone("high-score-table");
     }
     element("guessedNum").value = "";
-    element("startBTN").style.visibility = "hidden";
-    if (element("userName").value !== "") {
-        setDisplayNone("start-menu");
-        setDisplayVisible("game-grid");
-        app.userName = element("userName").value;
-        element("name-display-text").innerHTML = app.userName + " turn";
-        //TODO
-        getTheNumberToGuess();
+    setVisibilityOn("startBTN");
+    // if (element("userName").value !== "") {
+    setDisplayNone("start-menu");
+    setDisplayVisible("game-grid");
 
-        element("userName").value = "";
-        // element("high-score-table").style.display="none";
-    }
-    else {
+    //TODO
+    getTheNumberToGuess();
 
-        let worningText = element("name-worning")
-        worningText.innerText = "Error Enter your User Name ...!!!";
-        worningText.style.color = "red";
-    }
-    element("startBTN").style.visibility = "";
-}
-function restartTheGame() {
-    app.numberOfGuesses = 0;
-    app.guessesCount = 0;
-    app.guessesNumbers = [];
-    element("startBTN").style.visibility = "";
-    element("checkNumberBTN").style.visibility = "";
+    element("userName").value = "";
+    // element("high-score-table").style.display="none";
+    // }
+    // else {
 
+    //     let worningText = element("name-worning")
+    //     worningText.innerText = "Error Enter your User Name ...!!!";
+    //     worningText.style.color = "red";
+    // }
+    setVisibilityOn("startBTN");
 }
 
 
 function checkTheGuessedNumber() {
-    element("checkNumberBTN").style.visibility = "hidden";
+    setVisibilityOff("checkNumberBTN");
     const guessedNumber = element("guessedNum").value;
     if (guessedNumber % 1 != 0) {//check if the user entered a number and not characters
-        element("checked-number-worning").innerText = " error cannot enter anumber of 4 digets";
-        element("checkNumberBTN").style.visibility = "";
+        element("checked-number-worning").innerText = " Error what you entered is not anumber. enter a number of 4 different digets";
+        setVisibilityOn("checkNumberBTN");
     } else {
 
         if (guessedNumber.length === 4) {
-            app.guessesCount++;
-            app.guessesNumbers[app.guessesCount] = guessedNumber;
-            //
-            console.log(guessedNumber + " guess number " + app.guessesCount + " " + app.guessesNumbers);
-            //    
-            let result = "";
-            for (let i = 0; i < guessedNumber.length; i++) {
-                if (guessedNumber[i] == app.number[i]) {
-                    result += "<div style='background-color:green; color : white; display:inline; ' >0</div> <div style=' display:inline; ' > </div>";
-                } else {
-                    result += "<div style='background-color:red; color : white; display:inline; ' >0</div> <div style=' display:inline; ' > </div>";
-                }
-            }
-            let tableRow = "<tr><td>" + app.guessesCount + "</td><td>" + guessedNumber + "</td><td>" + result + "</td></tr>"
-            
 
-            element("guesses-table").innerHTML += tableRow;
-
-            if (guessedNumber == app.number) {
-                sendTheNumberOfGuessesToDataBase();// TODO
-                element("checkNumberBTN").style.visibility = "hidden";
-                app.guessesCount = 0;
-
+            if (!checkIfTheGuessedNumberIsContainsADifferentDigits(guessedNumber)) {
+                element("checked-number-worning").innerText = "Error !! Enter a Number With four Different Digits";
+                setVisibilityOn("checkNumberBTN");
             } else {
-                element("checked-number-worning").innerText = " worng number Enter anther one!!";
-                element("checkNumberBTN").style.visibility = "";
+                app.guessesCount++;
+                app.guessesNumbers[app.guessesCount] = guessedNumber;
+                //
+                console.log(guessedNumber + " guess number " + app.guessesCount + " " + app.guessesNumbers);
+                //    
+                let result = "";
+
+                for (let i = 0; i < guessedNumber.length; i++) {
+                    let isTheNumbersInTheSameDiget = false;
+                    let isTheNumbersEqualsAndNotInTheSameDigits = false;
+                    for (let j = 0; j < guessedNumber.length; j++) {
+
+                        if (guessedNumber[i] == app.number[j]) {
+                            isTheNumbersInTheSameDiget = i == j;
+                            isTheNumbersEqualsAndNotInTheSameDigits = i !== j;
+
+                        }
+                    }
+                    if (isTheNumbersInTheSameDiget) {
+                        result += "<div style='background-color:green; color : white; display:inline; ' >0</div> <div style=' display:inline; ' > </div>";
+                    } else if (isTheNumbersEqualsAndNotInTheSameDigits) {
+                        result += "<div style='background-color:red; color : white; display:inline; ' >0</div> <div style=' display:inline; ' > </div>";
+                    }
+
+                }
+
+
+                let tableRow = "";
+                if (guessedNumber == app.number) {
+                    sendTheNumberOfGuessesToDataBase();
+                    setVisibilityOff("checkNumberBTN");
+                    element("checked-number-worning").innerText = "Enter your name and submet the Results to send it to the database";
+                    app.guessesCount = 0;
+                    setDisplayVisible("nameSubmet");
+                    tableRow = "<tr style='background-color:#a6ff00;'><td>" + app.guessesCount + "</td><td>" + guessedNumber + "</td><td>" + result + "</td></tr>";
+
+                } else {
+                    element("checked-number-worning").innerText = " worng number Enter anther one!!";
+                    setVisibilityOn("checkNumberBTN");
+                    tableRow = "<tr><td>" + app.guessesCount + "</td><td>" + guessedNumber + "</td><td>" + result + "</td></tr>";
+                }
+
+                element("guesses-table").innerHTML += tableRow;
             }
+
 
         } else {
             element("checked-number-worning").innerText = " worng number !! number most be 4 digets Enter anther one!!";
-            element("checkNumberBTN").style.visibility = "";
+            setVisibilityOn("checkNumberBTN");
         }
-        
+
         if (app.guessesCount == app.timesToGuess) {
-            element("checkNumberBTN").style.visibility = "hidden";
-            element("checked-number-worning").innerText = " click the restart button your guesses are 20 guesses";
-            sendTheNumberOfGuessesToDataBase();
+            
+            setVisibilityOff("checkNumberBTN");
+            clearTheGame();
         }
     }
 }
 
+function checkIfTheGuessedNumberIsContainsADifferentDigits(number) {
 
+    let isTheNumberValide = "";
+    for (let digitIndex = 0; digitIndex < number.length; digitIndex++) {
+        let dublicatDigits = 0;
+        for (let j = 0; j < number.length; j++) {
+            if (number[digitIndex] == number[j]) {
+                dublicatDigits++;
+            }
+
+        }
+        if (dublicatDigits > 1) {
+            isTheNumberValide += "0";
+        } else {
+            isTheNumberValide += "1";
+        }
+
+    }
+    console.log(isTheNumberValide);
+    return isTheNumberValide === "1111";
+}
 
 function getTheNumberToGuess() { //TODO
-    app.number="0147";
+    app.number = "0147";
     //     let xhttp = new XMLHttpRequest();
     // xhttp.onreadystatechange = function() {
     // 	if (this.readyState == 4 && this.status == 200) {
@@ -125,9 +162,40 @@ function getTheNumberToGuess() { //TODO
 
 }
 
+
+function submetTheGameResults() {
+    setDisplayVisible("high-score-table");
+    setDisplayVisible("nameSubmet");
+    if (element("userName").value !== "") {
+        sendTheNumberOfGuessesToDataBase();
+    } else {
+        setDisplayNone("submetNamebtn");
+        let worningText = element("checked-number-worning")
+        worningText.innerText = "Error Enter your Name ...!!!";
+        worningText.style.color = "red";
+    }
+    sendTheNumberOfGuessesToDataBase();
+    clearTheGame();
+}
+
+function clearTheGame() {
+    app.userName = "",
+        getTheNumberToGuess();
+    app.guessesNumbers = [];
+    setVisibilityOn("checkNumberBTN");
+    
+    setDisplayVisible("submetNamebtn");
+    setDisplayNone("nameSubmet");
+    element("checked-number-worning").innerText = "guess  anther number";
+    element("guesses-table").innerHTML = " <tr> <th>#</th> <th>Guess</th> <th>Result</th></tr>";
+    element("guessedNum").value = "";
+    app.guessesCount = 0;
+
+}
+
 function sendTheNumberOfGuessesToDataBase() {
 
-    setDisplayVisible("high-score-table");
+
 
     const plyer = {
         name: app.name,
@@ -138,9 +206,9 @@ function sendTheNumberOfGuessesToDataBase() {
     //    let xhttp = new XMLHttpRequest();
     //        xhttp.onreadystatechange = function() {
     // 	    if (this.readyState == 4 && this.status == 200) {
-               console.log(this.responseText);
-               app.highScoreTable=JSON.parse( this.responseText);
-               creatTheHighScoreTable(app.highScoreTable);
+    // console.log(this.responseText);
+    // app.highScoreTable = JSON.parse(this.responseText);
+    creatTheHighScoreTable(app.highScoreTable);
     // 	}
     // };
     // xhttp.open("POST", app.baseURL + "/insertPlayer", true);
@@ -149,11 +217,39 @@ function sendTheNumberOfGuessesToDataBase() {
 }
 function creatTheHighScoreTable(highScoreTable) {
     for (let i = 0; i < highScoreTable.length; i++) {
-       // "<tr><td>" + app.guessesCount + "</td><td>" + guessedNumber + "</td><td>" + result + "</td></tr>"
-         const tableRow = "<tr><td>"+ i +"</td><td>" + highScoreTable[i].name + "</td><td>" + highScoreTable[i].numberOfGuesses + "</td></tr>;
+        // "<tr><td>" + app.guessesCount + "</td><td>" + guessedNumber + "</td><td>" + result + "</td></tr>"
+        const tableRow = "<tr><td>" + i + "</td><td>" + highScoreTable[i].name + "</td><td>" + highScoreTable[i].numberOfGuesses + "</td></tr>";
         element("high-score-table").innerHTML += tableRow;
     }
 }
+
+
+function clearHighScoreTable() {
+setDisplayVisible("admenPasswordId");
+getAdmenPassword();
+
+
+}
+function getAdmenPassword(){
+    //     let xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    // 	if (this.readyState == 4 && this.status == 200) {
+    // 		console.log("admen password is " + this.responseText);
+            // return this.responseText;
+    // 	}
+    // };
+    // xhttp.open("get", app.baseURL + "/getAdmenPasword", true);
+    // xhttp.setRequestHeader("Content-Type", "application/json");
+    // xhttp.send();
+}
+
+function setVisibilityOff(elementId) {
+    element(elementId).style.visibility ="hidden";
+}
+function setVisibilityOn(elementId) {
+    element(elementId).style.visibility ="";
+}
+
 
 function setDisplayVisible(elementId) {
     element(elementId).style.display = "";
